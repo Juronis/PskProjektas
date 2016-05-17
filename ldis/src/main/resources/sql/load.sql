a58e7668a5ebfe42f470f978d9723f730f7fd601
Vaidotas <vaidotas.juronis@gmail.com>
2016-05-17 19:48:03 +0300

    Pridėtas užkrovimas vasarnamio vietų skaičiaus.

--- ldis/src/main/resources/sql/load.sql
+++ ldis/src/main/resources/sql/load.sql
@@ -2,6 +2,6 @@ INSERT INTO MEMBER("id", "creditamount", "email", "isfacebookuser", "isadmin", "
 INSERT INTO MEMBER("id", "creditamount", "email", "isfacebookuser", "isadmin", "isfullmember", "username", "password") VALUES(nextval('hibernate_sequence'),'5' ,'vaidotas.juronis@gmail.com', true, true, true, 'marijus koreniuaskas', '458');
 INSERT INTO MEMBER("id", "creditamount", "email", "isfacebookuser", "isadmin", "isfullmember", "username", "password") VALUES(nextval('hibernate_sequence'),'5' ,'vaidotas.juronis@gmail.com', true, true, true, 'donatas donatelas', '456');
 INSERT INTO MEMBER("id", "creditamount", "email", "isfacebookuser", "isadmin", "isfullmember", "username", "password") VALUES(nextval('hibernate_sequence'),'5' ,'vaidotas.juronis@gmail.com', true, true, true, 'arnas jonaitis', '123');
-INSERT INTO SUMMERHOUSE ("id", "description") VALUES(nextval('hibernate_sequence'),'Labai grazus vasarnamis') ;
-INSERT INTO SUMMERHOUSE ("id", "description") VALUES(nextval('hibernate_sequence'),'Dar grazesnis vasarnamis') ;
-INSERT INTO SUMMERHOUSE ("id", "description") VALUES(nextval('hibernate_sequence'),'Pats tas vasarnamis') ;
+INSERT INTO SUMMERHOUSE ("id", "description", "numberofplaces") VALUES(nextval('hibernate_sequence'),'Labai grazus vasarnamis', 5) ;
+INSERT INTO SUMMERHOUSE ("id", "description", "numberofplaces") VALUES(nextval('hibernate_sequence'),'Dar grazesnis vasarnamis', 10) ;
+INSERT INTO SUMMERHOUSE ("id", "description", "numberofplaces") VALUES(nextval('hibernate_sequence'),'Pats tas vasarnamis', 14) ;
