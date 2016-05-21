c320f3283249191abeca45d77bb53f1c4f7ff33e
motasjungas <motas.jungas84369@gmail.com>
2016-05-21 13:00:01 +0300

    pakeista i mysql sintakse

--- ldis/src/main/resources/sql/drop.sql
+++ ldis/src/main/resources/sql/drop.sql
@@ -1,4 +1,4 @@
 drop table if exists Reservation cascade;
 drop table if exists MEMBER cascade;
 drop table if exists Summerhouse cascade;
-drop table if exists extraactivities cascade;
+drop table if exists EXTRAACTIVITIES cascade;
